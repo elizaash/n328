e808243b10401e8149bc641f51c3cb623a8fda2b
elizaash <70230927+elizaash@users.noreply.github.com>
2021-03-14 00:28:56 -0500

    Update

--- indexTwo.js
+++ indexTwo.js
@@ -12,7 +12,7 @@ var svg = d3.select("#my_dataviz")
   .append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
-    .append("g")
+  .append("g")
     .attr("transform",
           "translate(" + margin.left + "," + margin.top + ")");
 
